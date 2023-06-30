1795c2445cbf12706a528b958148fd0625896a74
suraj-singh127 <surajsingh25163@gmail.com>
2023-06-30 12:23:40 +0530

    added files to gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use pcap::{Device, Capture};
-use pktparse::{*, tcp::TcpHeader, udp::UdpHeader, ipv4::IPv4Header, ipv6::IPv6Header, arp::ArpPacket, ethernet::{EtherType, EthernetFrame}, ip::IPProtocol};
-use tls_parser::TlsMessage;
+use pktparse::{*, tcp::TcpHeader, udp::UdpHeader, ipv4::IPv4Header, ipv6::IPv6Header, arp::ArpPacket, ethernet::{EtherType, EthernetFrame, MacAddress}, ip::IPProtocol};
+use tls_parser::{TlsMessage, nom::{AsBytes}};
 use clap::{Parser};
 
 
@@ -10,6 +10,8 @@ fn main() {
     #[command(name = "sniffit")]
     #[command(version = "1.0")]
     #[command(about = "Capturing packets from interface.")]
+
+    //Struct for parsing the arguments
     pub struct Cli{
         arg1 : Option<String>,
         arg2 : Option<String>,
@@ -20,6 +22,7 @@ fn main() {
         err_count: u64,
     }
 
+    //Implementing the CapturePacket struct
     impl CapturePacket {
         pub fn new() -> CapturePacket {
             CapturePacket { err_count: 0 }
@@ -56,16 +59,29 @@ fn main() {
             println!("To capture packets from a device - cargo run capture [device_serial number]");
         }
 
+        //Takes the serial number of the device in form of unsigned 8 bit unsigned integer 0-255
         pub fn print_to_console(&self , device_number:u8) {
 
             let mut device_name = String::from("");
             let mut device_description = "".to_string();
 
+            //Looking for all the interfaces on the devices
+            //Device::list() -> Returns a Result<T,E> type where -
+            //T - Vector of Devices
+            //E - Returns error if encountered
             let list = Device::list();
+
+            //for device selection
             let mut serial = 1;
+
+            //Matching the list to look for the serial number passed as an argument
             match list {
+                //Vector of devices
                 Ok(devices) => {
+                    //Device name of zeroth interface
                     device_name = devices[0].name.clone();
+
+                    //Device desc of first device if there is some description
                     device_description = match &devices[0].desc {
                         Some(desc) => {
                             desc.to_string()
@@ -73,7 +89,10 @@ fn main() {
                         None => "Not Available".to_string()
                     };
 
+                    //Matching the arguments device serial number with actual device
                     for device in devices {
+
+                        //if the serial number matches store the device name and device description
                         if serial == device_number {
                             device_name = device.name;
                             device_description = device.desc.unwrap_or(String::from("Device name not available"));
@@ -81,45 +100,136 @@ fn main() {
                         serial = serial + 1;
                     }
                 }
+                //Else if error is encountered
                 Err(err) => {
                     eprintln!("{:?}",err);
                 }
             }
             
+            //First a Capture<Inactive> handle is opened by passing the device or device name to Capture::from_device - pcap rust docs
             let cap2 = Capture::from_device(device_name.as_str()).expect("error capturing");
-            let mut cap_handle = cap2.promisc(true).snaplen(5000).timeout(10000).open().unwrap();
+            
+            //Then the Capture<Inactive> is activated via .open() function and setting other configurations 
+            let mut cap_handle = cap2.promisc(true).snaplen(100).timeout(10000).open().unwrap();
+
+            //Once the device ready for Active Capturing we can list datalinks and use them accordingly
             let data_links = cap_handle.list_datalinks().expect("could not find");
-            println!("\n=========================================================");
+            let symbol = "*";
+            let width = 500;
+            println!("\n===========================================================================================");
             println!("Links Available are - ");
             for links in data_links{
                 println!("{:?}",links.get_description().unwrap());
             }
-            println!("\n=========================================================");
+            println!("\n===========================================================================================");
             println!("Capturing packets from - {}" ,device_description);
+            
+            //For counting the number of packets
+            let mut packet_no:i64 = 1;
 
+            //Now we can traverse over the captured packets by calling active capture handles .next() function            
             while let Ok(packet) = cap_handle.next() {
                 let data = packet.data.to_owned();
                 let len = packet.header.len;
+
+                //Formaitng the timestamps to be shown upto 6 digit of decimal 
                 let ts: String = format!(
                     "{}.{:06}",
                     &packet.header.ts.tv_sec, &packet.header.ts.tv_usec
                 );
+
+                //Creating a new instance of Parsed Packet
                 let packet_parse = ParsedPacket::new();
                 let parsed_packet = packet_parse.parse_packet(data, len, ts);
-                match parsed_packet {
-                    Ok(packet) => {
-                        println!("{:?}",packet.headers[1]);
-                    },
-                    Err(e) => {
-                        println!("{:?},Error Count = {} ", e,self.err_count);
+                self.print_packet(parsed_packet,packet_no);
+                packet_no = packet_no + 1;
+            }
+        }
+
+        //Printing individual packets to console
+        pub fn print_packet(&self,parsed_packet:Result<ParsedPacket,String>,packet_no:i64){
+            println!("\n===========================================================================================\n");
+            
+            print!("\n==================================== Packet no. {} ==========================================\n\n",packet_no);
+            
+            match parsed_packet {
+                Ok(parsed_packet) => {
+                    //parsed packet is passed to function for printing header info
+                   &self.header_information(parsed_packet);
+                }
+                Err(er)=> {
+                    eprintln!("{}",er);
+                }
+            }
+            println!("\n===========================================================================================");
+        }
+
+        //Function for getting the header information from parsed packet
+        pub fn header_information(&self,packet:ParsedPacket) {
+            print!("\n============================== {} ==============\n\n",packet.timestamp);
+            for header in packet.headers {
+                match header {
+                    //Defining action for EthernetFrame header
+                    PacketHeader::Ether(ethernet_frame) => {
+                        let src = format!("{:02X?}",ethernet_frame.source_mac.0.as_bytes());
+                        let dst = format!("{:02X?}",ethernet_frame.dest_mac.0.as_bytes());
+                        println!("Source MAC: {}  -----> Destination MAC: {}",&src,&dst);
+                    }
+
+                    //For TLS Messages
+                    PacketHeader::Tls(tls_message) => {
+                        println!("TLS Message type - {:?}",tls_message);
+                    }
+                    
+                    //For IPv4
+                    PacketHeader::IPv4(ipv4_header) => {
+                        println!("Version - {}  IHL - {}",ipv4_header.version,ipv4_header.ihl);
+                        println!("Length - {}   Protocol - {:?}", ipv4_header.length,ipv4_header.protocol);
+                        println!("Source Address: {}  -----> Destination Address: {}",ipv4_header.source_addr,ipv4_header.dest_addr);
+                    }
+
+                    //for IPv6
+                    PacketHeader::IPv6(ipv6_header)=> {
+                        
+                        println!("Version - {}  Hop Limit - {}", ipv6_header.version,ipv6_header.hop_limit);
+                        println!("Length - {}   Protocol - {:?}", ipv6_header.length,ipv6_header.next_header);
+                        println!("Source Address: {}  -----> Destination Address: {}",ipv6_header.source_addr,ipv6_header.dest_addr);
+                    }
+
+                    //For Arp header - if the packet uses ARP protocol
+                    PacketHeader::Arp(arp_packet)=> {
+                        
+                        println!("Hardware Addr Type:{}   Protocol Address Type:{:?}", arp_packet.hw_addr_size,arp_packet.proto_addr_type);
+                        println!("Operation: {:?}",arp_packet.operation);
+                        println!("Source MAC: {:?}  -----> Destination MAC: {:?}",arp_packet.src_mac,arp_packet.dest_mac);
+                    }
+
+                    //For printing TCP Header information
+                    PacketHeader::Tcp(tcp_header) => {
+                        
+                        println!("Source Port: {} -----> Dest Port: {}",tcp_header.source_port,tcp_header.dest_port);
+                        println!("Sequence No: {}   Ack No: {}",tcp_header.sequence_no,tcp_header.ack_no);
+                        println!("FLAGS -   ACK: {}   FIN: {}   PSH: {}    RST: {}    SYN: {}   URG:{}",
+                        &self.convert_flag(tcp_header.flag_ack),&self.convert_flag(tcp_header.flag_fin),
+                        &self.convert_flag(tcp_header.flag_psh),&self.convert_flag(tcp_header.flag_rst),
+                        &self.convert_flag(tcp_header.flag_syn),&self.convert_flag(tcp_header.flag_urg));
+                        
+                    }
+
+                    //For printing UDP Header information
+                    PacketHeader::UDP(udp_header) => {
+                        println!("Source Port: {} -----> Dest Port: {}",udp_header.source_port,udp_header.dest_port);
+                        println!("Length: {}    Checksum: {}",udp_header.length,udp_header.checksum);
                     }
                 }
             }
         }
-
-        pub fn print_packet(parsed_packet:ParsedPacket){
-            
+        
+        //For converting bool flag to 0 and 1
+        pub fn convert_flag(&self,flag:bool) -> u8{
+            flag as u8
         }
+        
     }
     
     //Enumeration for headers of different header types
@@ -216,6 +326,7 @@ fn main() {
                        EtherType::IPv6 => {
                             self.parsing_ipv6(content, &mut parsed_packet)?;
                        }
+                       //if ethertype is arp
                        EtherType::ARP => {
                             self.parsing_arp(content, &mut parsed_packet)?;
                        }
@@ -409,23 +520,37 @@ fn main() {
 
     }
 
+    //Creating an instance of Cli for parsing arguments
     let arguments = Cli::parse();
+
+    //Creating a new instance of CapturePacket
     let capture_packets = CapturePacket::new();
     
+    //Matching arguments with the provided arguments
     match arguments.arg1 {
         Some(arg) => {
+            //If the first argument is list
             if arg=="list"{
                 capture_packets.print_devices();
             }
+            //if it is capture
             else if arg == "capture" {
+                //if capture is called then check the third argument  
                 if let Some(device_number) = arguments.arg2.as_deref() {
+
+                    //pass the number to start capturing packets
                     let number:u8  = device_number.parse().unwrap();
+
+                    //if the number passed is out of range or not matching
+                    //Default or 0th device will be selected automatically
                     capture_packets.print_to_console(number);
                 }
                 else{
+                    //If no serial is provided
                     println!("device selected is unavailable");
                 }
             }
+            //in case of invalid arguments
             else {
                 println!("Invalid arguments try again");
             }       
